c3457773269916e6fbe44a287d5a27656ede0344
suvanshenoy <suvanshenoy155@gmail.com>
2025-05-15 03:09:42 +0530

    fix: move subdirectories of src directory to the root directory

--- frontend/src/pages/Budgeting.jsx
+++
@@ -1,3 +0,0 @@
-const Budgeting = () => {};
-
-export default Budgeting;
--- frontend/src/store/index.jsx
+++
@@ -1,3 +0,0 @@
-import { create } from "zustand";
-import { useBudgetStore } from "./budgetStore.jsx";
-import { useAnswerStore } from "./answerStore.jsx";
