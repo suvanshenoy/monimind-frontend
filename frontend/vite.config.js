f8e38852d530161b8d7149f38cb975cafbeafd4b
suvanshenoy <suvanshenoy155@gmail.com>
2025-05-15 03:08:01 +0530

    feat: add alias in jsconfig.json and vite.config.js according to new directory structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": "./",
+			"@page": resolve(__dirname, "./page"),
+			"@layout": resolve(__dirname, "./layout"),
+			"@component": resolve(__dirname, "./component"),
+			"@store": resolve(__dirname, "./store"),
+		},
+	},
 	server: {
 		port: 3000,
 	},
